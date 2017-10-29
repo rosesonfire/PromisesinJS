840351357b40f33974cabf3585846d62c522a9cd
ishti_ik <ishti_iks@yahoo.com>
2017-10-29 14:00:53 +0600

    add more tests

--- promise.js
+++ promise.js
@@ -1,17 +1,17 @@
 const { XMLHttpRequest } = require('xmlhttprequest');
 
-const url = "http://localhost:8080/service/etc";
+const url = "/";
 
-function asyncCaller(url, callback) {
+function httpCaller(url, callback, async) {
 
     var xhttp = new XMLHttpRequest();
     
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            callback(xhttp.responseText);
+            callback("here");
         }
     };
-    xhttp.open("GET", url, true);
+    xhttp.open("GET", url, async);
     xhttp.send();
 }
 
@@ -23,70 +23,172 @@ function MyPromise(someAsyncWork, successC, failureC) {
     this.successCallBack = successC;
     this.failureCallBack = failureC;
 
-    this.success = function(successR) {
-        this.status="Succeeded";
-        this.response = successR;
-        let modifiedResponse = successR;
+    this.success = function(successR) {        
         if (this.successCallBack) {
-            modifiedResponse = this.successCallBack(successR);
+            try {
+                this.response = this.successCallBack(successR);
+                this.callChildSuccesses();
+            } catch (e) {
+                this.response = e;
+                this.callChildFailures();
+            }
+        } else {
+            this.response = successR;
+            this.callChildSuccesses();
         }
-        for (let I = 0; I < this.subs.length; I++) {
-            this.subs[I].success(modifiedResponse);
-        }
-    }
+    };
 
     this.failure = function(failureR) {
-        this.status="Failed";
-        this.response = failureR;
-        let modifiedResponse = failureR;
         if (this.failureCallBack) {
-            modifiedResponse = this.failureCallBack(failureR);
-        }
-        for (let I = 0; I < this.subs.length; I++) {
-            this.subs[I].failure(modifiedResponse);
-        }
-    }
+            try {
+                this.response = this.failureCallBack(failureR);
+            } catch (e) {
+                this.response = e;
+            }
+        } else {
+            this.response = failureR;
+        }     
+        this.callChildFailures();
+    };
 
     this.then = function(successCallBack, failureCallBack) {
         const newSub = new MyPromise(false, successCallBack, failureCallBack);
         this.subs.push(newSub);
         if (this.status === "Succeeded") {
-            newSub.succeed(this.response);
+            newSub.success(this.response);
         } else if (this.status === "Failed") {
-            newSub.fail(this.response);
+            newSub.failure(this.response);
         }
         return newSub;
-    }
+    };
+
+    this.callChildSuccesses = function() {
+        this.status="Succeeded";
+        for (let I = 0; I < this.subs.length; I++) {
+            this.subs[I].success(this.response);
+        }
+    };
+
+    this.callChildFailures = function() {
+        this.status="Failed";
+        for (let I = 0; I < this.subs.length; I++) {
+            this.subs[I].failure(modifiedResponse);
+        }
+    };
 
     if (someAsyncWork) {
-        someAsyncWork(this.success.bind(this), this.failure.bind(this));    
+        try {
+            someAsyncWork(this.success.bind(this), this.failure.bind(this));
+        } catch (e) {
+            this.failure(e);
+        }
     }
 }
 
-let result = "still not populated, this proves that this is asycn.";
+function test1() {
 
-new MyPromise(function(successFunc, failureFunc){
-    asyncCaller(url, function(value){
-        // this is the part that we have to do on our own,
-        // the promise object does not know how to do it.
-        if (value) {
-            successFunc(value);
-        } else {
-            failureFunc(value);
-        }
+    console.log("test1");
+
+    let result = "still not populated, this proves that this is asycn.";
+
+    new MyPromise(function(successFunc, failureFunc){
+        httpCaller(url, function(value){
+            // this is the part that we have to do on our own,
+            // the promise object does not know how to do it.
+            if (value) {
+                successFunc(value);
+            } else {
+                failureFunc(value);
+            }
+        }, true);
+    }).then(function(response){
+        return response + " Adding 1";
+    }).then(function(response){
+        return response + " Adding 2";
+    }).then(function(response){
+        return response + " Adding 3";
+    }).then(function(response){
+        return response + " Adding 4";
+    }).then(function(response){
+        result = response;
+    }).then(function(response){
+        console.log(result);
     });
-}).then(function(response){
-    return response + " Adding 1";
-}).then(function(response){
-    return response + " Adding 2";
-}).then(function(response){
-    return response + " Adding 3";
-}).then(function(response){
-    return response + " Adding 4";
-}).then(function(response){
-    result = response;
-}).then(function(response){
-    console.log(result);
-});
 
-console.log(result);
+    console.log(result);
+}
+
+function test2() {
+
+    console.log("test2");
+
+    let result = "still not populated.";
+
+    new MyPromise(function(successFunc, failureFunc){
+        httpCaller(url, function(value){
+            // this is the part that we have to do on our own,
+            // the promise object does not know how to do it.
+            if (value) {
+                successFunc(value);
+            } else {
+                failureFunc(value);
+            }
+        }, false);
+    }).then(function(response){
+        return response + " Adding 1";
+    }).then(function(response){
+        return response + " Adding 2";
+    }).then(function(response){
+        return response + " Adding 3";
+    }).then(function(response){
+        return response + " Adding 4";
+    }).then(function(response){
+        result = response;
+    }).then(function(response){
+        console.log(result);
+    });
+
+    console.log(result);
+}
+
+function test3() {
+
+    console.log("test3");
+
+    const myTask = new MyPromise(function(successFunc, failureFunc){
+        httpCaller(url, function(value){
+            // this is the part that we have to do on our own,
+            // the promise object does not know how to do it.
+            if (value) {
+                successFunc(value);
+            } else {
+                failureFunc(value);
+            }
+        }, true);
+    });
+
+    const mySecondTask = myTask.then(function(response){
+        return response + " Adding 1";
+    });
+
+    myTask.then(function(response){
+        console.log(response + " Adding 2");
+    });
+
+    mySecondTask.then(function(response){
+        console.log(response + " Adding 3");
+    });
+
+    mySecondTask.then(function(response){
+        console.log(response + " Adding 4");
+    });
+}
+
+// if first task is async and rest are sync
+test1();
+
+// if all the tasks are sync
+// test2();
+
+// multiple then
+// test3();
